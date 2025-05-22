c936dd0119b838b953a8802fdba15bc5bcf74db8
gcdavidq <gian.quezada@upch.pe>
2025-05-21 19:00:18 -0500

    Actualizacion archivos detectar anomalias

---
+++ VOTACION/deteccion/detectar_anomalias.cpp
@@ -0,0 +1,17 @@
+/*
+
+*/
+
+#include <omp.h>
+#include <thread>
+#include <chrono>
+#include <functional> // Para std::hash
+#include <VOTACION/deteccion/detectar_anomalias.hpp>
+#include <VOTACION/common/estructura_votos.hpp>
+
+using namespace std;
+
+namespace deteccion {
+
+    
+}
